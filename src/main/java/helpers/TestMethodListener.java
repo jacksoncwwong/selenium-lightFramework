156ec31d8be7d234d61adf53254648a085c87440
Jackson Wong <jackson.cw.wong@gmail.com>
2018-06-27 15:50:54 -0400

    one more minor change

--- src/main/java/helpers/TestMethodListener.java
+++ src/main/java/helpers/TestMethodListener.java
@@ -9,9 +9,6 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-import java.io.StringWriter;
-import java.io.PrintWriter;
-
 public class TestMethodListener implements ITestListener, ISuiteListener, IInvokedMethodListener {
 
     // ISuiteListener and will execute before the Suite start
--- src/main/java/tests/ExtendBase.java
+++ src/main/java/tests/ExtendBase.java
@@ -4,8 +4,6 @@ import helpers.BaseTest;
 import helpers.ExcelUtil;
 import helpers.SharedInfo;
 import org.testng.Assert;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class ExtendBase extends BaseTest {
