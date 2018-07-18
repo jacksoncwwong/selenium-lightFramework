f05b57126d57ee577de672a239427f0af2ac52be
Jackson Wong <jackson.cw.wong@gmail.com>
2018-07-18 12:14:23 -0400

    more revisions

---
+++ src/main/java/pages/SamplePage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class SamplePage {
+    final WebDriver driver;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"qacookies-continue-button\"]")
+    public WebElement continueBtn;
+
+    public SamplePage (WebDriver driver) {
+        this.driver = driver;
+    }
+}
--- src/main/java/tests/ExtendBase.java
+++ src/main/java/tests/ExtendBase.java
@@ -3,31 +3,38 @@ package tests;
 import helpers.BaseTest;
 import helpers.ExcelUtil;
 import helpers.SharedInfo;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.SamplePage;
 
 public class ExtendBase extends BaseTest {
+    static SamplePage SamplePage = PageFactory.initElements(getDriver(), SamplePage.class);
+
     @Test
     public void extendBaseFlow() {
-        //can change environment here for each of the tests
-//        SharedInfo.env = "QA";
+        //can change environment for each of the tests like so:
+        SharedInfo.env = "QA";
+        //declaring test name below
         SharedInfo.testName = "extendBaseFlow";
 
+        //this line sets up your spreadsheets:
         ExcelUtil.setTestExcelData();
+        //this line sets up your environment and login credentials:
         setupEnv("");
 
-        //start writing your test from here, the line below is just something to trigger a failure, please remember to remove when writing your own test
-        Assert.assertTrue(false);
-
-        //below is an example of logging in
-//        LandingPage.signInBtn.click();
-//
-//        shortWait();
-//
-//        writeData("", "", "loginUser is " + loginUser);
-//        writeData("", "", "loginPwd is " + loginPwd);
-//
-//        SignInPage.signInIDInput.sendKeys(loginUser);
+        //*** most likely start writing your test from here ***
+        //loginUser and loginPwd should be set at this point, you can use them like so:
+        //(the code below is just an example, it doesn't work)
+//        SignInPage.signInUserInput.sendKeys(loginUser);
 //        SignInPage.signInPwdInput.sendKeys(loginPwd);
+
+        //example of how to call elements from SamplePage
+        //the line below only works if SharedInfo.env = "QA":
+        SamplePage.continueBtn.click();
+
+        //the line below is just something to trigger a failure
+        //run the test as is to demonstrate what a failed report would look like
+        Assert.assertTrue(false);
     }
 }
